480fabea0e8f0b1830afbdf150537ba8f52df707
Lookotza <lucaathanasiu@gmail.com>
2021-04-26 23:13:38 +0300

    finally works

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
 const ITEMS_PACK_URL = 'https://gist.githubusercontent.com/ctrl-raul/3b5669e4246bc2d7dc669d484db89062/raw/';
-const itemsPack = await getJson(ITEMS_PACK_URL);
+let itemsPack = null;
+let items = [];
 
-async function getJson () {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (err) {
+fetch(ITEMS_PACK_URL)
+    .then(response => response.json())
+    .then(json => {
+        itemsPack = json;
+        // foobar gonna use this block more in future doodoodaa
+        items = itemsPack.items;
+    })
+    .catch(err => {
         // For now we just re-throw the error, delete
         // the next code line if you wanna do something
         // else in case it fails to load the JSON
         throw err;
-    }
-}
-let items = myItemsPack.items;
+    });
+
+
 
 const openBoxBtn = document.getElementById('open-box-btn');
-openBoxBtn.onclick = () => box();
+openBoxBtn.onclick = () => openBox();
 
 
 
 
 function openBox() {
 
-    const img = document.createElement('img');
+const img = document.createElement('img');
 
-    img.src = box();
+img.src = box();
 
-    document.body.appendChild(img);
+const previousImg = document.getElementById('item-img');
+if (previousImg !== null) {
+  previousImg.remove();
+}
+
+document.body.appendChild(img);
+
+img.id = 'item-img'
 }
 
 function getTiers(tier) {
